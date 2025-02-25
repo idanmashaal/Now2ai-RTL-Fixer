ce638a05fd76b2d57861f8860a53dcc18980bc8e
Idan Mashaal <idanmashaal@gmail.com>
2025-02-25 14:50:49 +0200

    added subfolder in dist per env

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
-const ZipPlugin = require("zip-webpack-plugin");
+const ZipPlugin = require("zip-webpack-plugin"); // Import the ZipPlugin
 
 // Create a timestamp for the build folder
 const getTimestamp = () => {
@@ -18,45 +18,54 @@ const getTimestamp = () => {
 
 const buildFolder = `build_${getTimestamp()}`;
 
-module.exports = {
-  mode: process.env.NODE_ENV === "production" ? "production" : "development",
-  entry: {
-    "content-script": "./src/content-script.js",
-    background: "./src/background.js",
-    popup: "./src/popup.js",
-  },
-  output: {
-    path: path.resolve(__dirname, "dist", buildFolder),
-    filename: "[name].js",
-    // Don't clean the entire dist directory, just the specific build folder
-    clean: true,
-  },
-  devtool: "cheap-module-source-map",
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: ["@babel/preset-env"],
+// Function to determine the output path based on the mode
+const getOutputPath = (mode) => {
+  return path.resolve(__dirname, "dist", mode, buildFolder);
+};
+
+module.exports = (env, argv) => {
+  const mode = argv.mode; // Get the mode from the webpack command
+  const outputPath = getOutputPath(mode); // Determine the output path
+
+  return {
+    mode: mode,
+    entry: {
+      "content-script": "./src/content-script.js",
+      background: "./src/background.js",
+      popup: "./src/popup.js",
+    },
+    output: {
+      path: outputPath, // Set the output path dynamically
+      filename: "[name].js",
+      clean: true,
+    },
+    devtool: "cheap-module-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
           },
         },
-      },
-    ],
-  },
-  plugins: [
-    new CopyPlugin({
-      patterns: [
-        { from: "manifest.json", to: "manifest.json" },
-        { from: "icons", to: "icons" },
-        { from: "src/popup.html", to: "popup.html" },
       ],
-    }),
-    new ZipPlugin({
-      path: path.resolve(__dirname, "dist"),
-      filename: buildFolder + ".zip",
-    }),
-  ],
+    },
+    plugins: [
+      new CopyPlugin({
+        patterns: [
+          { from: "manifest.json", to: "manifest.json" },
+          { from: "icons", to: "icons" },
+          { from: "src/popup.html", to: "popup.html" },
+        ],
+      }),
+      new ZipPlugin({
+        path: path.resolve(__dirname, "dist", mode), // Output zip in the mode subfolder
+        filename: buildFolder + ".zip",
+      }),
+    ],
+  };
 };
